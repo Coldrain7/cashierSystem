3f9006ad27d9c357588d0a8e952429a120508ed9
Coldrain7 <2057787038@qq.com>
2024-02-18 17:59:01 +0800

    进行数据库排序查找用到的数据结构，包含检查前端数据是否符合条件等方法

---
+++ src/main/java/com/example/mybatisplus/model/dto/SortDTO.java
@@ -0,0 +1,65 @@
+package com.example.mybatisplus.model.dto;
+
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class SortDTO {
+    private String prop;
+    private String order;
+    private Set<String> propSet;
+    public SortDTO(){
+        propSet = new HashSet<>();
+        propSet.add("name");
+        propSet.add("barcode");
+        propSet.add("inventory");
+        propSet.add("purchase_price");
+        propSet.add("price");
+        propSet.add("wholesale_price");
+        propSet.add("supplier.name");
+        propSet.add("produce_date");
+        propSet.add("expiration_time");
+        propSet.add("create_time");
+    }
+    /**
+     * 装换大写字母为下划线加小写字母
+     */
+    private void convertProp(){
+        if (prop == null) {
+            return;
+        }
+        String result = new String();
+        for (int i = 0; i < prop.length(); i++) {
+            char c = prop.charAt(i);
+            if (Character.isUpperCase(c)) {
+                result = result + "_" + Character.toLowerCase(c);
+            } else {
+                result += c;
+            }
+        }
+        prop = result;
+    }
+
+    /**
+     * 转化前端传来的字段为sql查询需要的形式，并判断是否能进行排序
+     * @return boolean
+     */
+    public boolean isContain(){
+        convertProp();
+        boolean flag = false;
+        if(order == null) return false;
+        if(order.equals("ascending")){
+            order = "asc";
+            flag = propSet.contains(prop);
+        }else if(order.equals("descending")){
+            order = "desc";
+            flag = propSet.contains(prop);
+        }
+        if(!prop.equals("supplier.name")) {
+            prop = "commodity." + prop;
+        }
+        return flag;
+    }
+}

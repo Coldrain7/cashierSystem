41bcf5a28e2d2764a905038273ada62752458464
Coldrain7 <2057787038@qq.com>
2024-03-05 00:51:16 +0800

    增加了获取所有店铺供应商的方法

--- src/main/java/com/example/mybatisplus/mapper/SupplierMapper.java
+++ src/main/java/com/example/mybatisplus/mapper/SupplierMapper.java
@@ -2,7 +2,11 @@ package com.example.mybatisplus.mapper;
 
 import com.example.mybatisplus.model.domain.Supplier;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatisplus.model.dto.SupplierDTO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +18,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SupplierMapper extends BaseMapper<Supplier> {
-
+    List<SupplierDTO> getSupplierList(@Param("supId")int supId);
 }

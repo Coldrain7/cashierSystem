6987dd7d7ec27eb9bb01fbd1f604faf258fde46c
Coldrain7 <2057787038@qq.com>
2024-04-16 01:07:05 +0800

    增加了recordVO用于传递给前端需要查看的数据

---
+++ src/main/java/com/example/mybatisplus/model/vo/recordVO.java
@@ -0,0 +1,17 @@
+package com.example.mybatisplus.model.vo;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class recordVO {
+    private Double totalNumber;//总数量
+    private Double totalPayment;//总收款金额
+    private Integer recordNum;//订单数量
+    private Double totalPurchasePrice;//总进价
+    private LocalDate beginDate;//
+    private LocalDate endDate;
+    private Double totalProfit;//总利润
+    private Double profitRatio;//利润率
+}

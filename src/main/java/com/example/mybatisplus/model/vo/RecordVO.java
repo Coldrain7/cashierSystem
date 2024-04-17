6362bd735f8af2cc52d747ecffd02c08dbcb154b
Coldrain7 <2057787038@qq.com>
2024-04-18 00:40:22 +0800

    重命名了文件

--- src/main/java/com/example/mybatisplus/model/vo/RecordVO.java
+++ src/main/java/com/example/mybatisplus/model/vo/RecordVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Data
-public class recordVO {
+public class RecordVO {
     private Double totalNumber;//总数量
     private Double totalPayment;//总收款金额
     private Integer recordNum;//订单数量
@@ -14,4 +14,5 @@ public class recordVO {
     private LocalDate endDate;
     private Double totalProfit;//总利润
     private Double profitRatio;//利润率
+    private String week;//记录第几周
 }
